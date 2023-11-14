3bb1161b5c3ebdb6c6aa3379be3012a8e34f4f1a
Faustharus <chailloleau-damien@outlook.fr>
2023-11-14 14:09:48 +0100

    Initial Commit

---
+++ Measurement/MeasurementApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeasurementApp.swift
+//  Measurement
+//
+//  Created by Damien Chailloleau on 14/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MeasurementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
